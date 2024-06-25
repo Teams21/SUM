9f3db4eb8affda96b57a01719fd7a5c3bda8d79a
Marcin <s22678@pjwstk.edu.pl>
2024-06-25 22:26:33 +0200

    Adjust code for pylint

--- app.py
+++ app.py
@@ -1,80 +1,10 @@
-import streamlit as st
+"""_summary_
+Create a prediction from the model based on the input paramameters from the app.
+"""
 import pickle
-import pandas as pd
 from datetime import datetime
-
-# Load the model
-filename = 'ml_models/banking_model.h5'
-model = pickle.load(open(filename, 'rb'))
-
-# Dictionaries to map integer values to categorical values
-job_d = {
-    0: 'admin.',
-    1: 'blue-collar',
-    2: 'entrepreneur',
-    3: 'housemaid',
-    4: 'management',
-    5: 'retired',
-    6: 'self-employed',
-    7: 'services',
-    8: 'student',
-    9: 'technician',
-    10: 'unemployed',
-    11: 'unknown'
-}
-
-education_d = {
-    0: 'primary',
-    1: 'secondary',
-    2: 'tertiary',
-    3: 'unknown'
-}
-
-default_d = {
-    0: 'no',
-    1: 'yes'
-}
-
-housing_d = {
-    0: 'no',
-    1: 'yes'
-}
-
-loan_d = {
-    0: 'no',
-    1: 'yes'
-}
-
-contact_d = {
-    0: 'cellular',
-    1: 'telephone',
-    2: 'unknown'
-}
-
-month_d = {
-    0: 'apr',
-    1: 'aug',
-    2: 'dec',
-    3: 'feb',
-    4: 'jan',
-    5: 'jul',
-    6: 'jun',
-    7: 'mar',
-    8: 'may',
-    9: 'nov',
-    10: 'oct',
-    11: 'sep'
-}
-
-poutcome_d = {
-    0: 'failure',
-    1: 'other',
-    2: 'success',
-    3: 'unknown'
-}
-
-# Title of the application
-title = 'Will the customer take advantage of the bank\'s offer?'
+import streamlit as st
+import pandas as pd
 
 # Initialize an empty DataFrame to store data
 if 'df' not in st.session_state:
@@ -84,6 +14,80 @@ if 'df' not in st.session_state:
     ])
 
 def main():
+    """_summary_
+    Main function of the application.
+    Create a prediction from the model based on the input paramameters from the app.
+    """
+    # Dictionaries to map integer values to categorical values
+    app_inputs = {
+        'job_d': {
+            0: 'admin.',
+            1: 'blue-collar',
+            2: 'entrepreneur',
+            3: 'housemaid',
+            4: 'management',
+            5: 'retired',
+            6: 'self-employed',
+            7: 'services',
+            8: 'student',
+            9: 'technician',
+            10: 'unemployed',
+            11: 'unknown'
+        },
+        'education_d': {
+            0: 'primary',
+            1: 'secondary',
+            2: 'tertiary',
+            3: 'unknown'
+        },
+
+        'default_d': {
+            0: 'no',
+            1: 'yes'
+        },
+
+        'housing_d': {
+            0: 'no',
+            1: 'yes'
+        },
+
+        'loan_d': {
+            0: 'no',
+            1: 'yes'
+        },
+
+        'contact_d': {
+            0: 'cellular',
+            1: 'telephone',
+            2: 'unknown'
+        },
+
+        'month_d': {
+            0: 'apr',
+            1: 'aug',
+            2: 'dec',
+            3: 'feb',
+            4: 'jan',
+            5: 'jul',
+            6: 'jun',
+            7: 'mar',
+            8: 'may',
+            9: 'nov',
+            10: 'oct',
+            11: 'sep'
+        },
+
+        'poutcome_d': {
+            0: 'failure',
+            1: 'other',
+            2: 'success',
+            3: 'unknown'
+        }
+    }
+
+
+    # Title of the application
+    title = 'Will the customer take advantage of the bank\'s offer?'
     # Set the page configuration
     st.set_page_config(page_title=title)
 
@@ -92,25 +96,47 @@ def main():
     left, right = st.columns(2)
     prediction = st.container()
 
-    # Display an image
-    st.image(
-        'https://media1.popsugar-assets.com/files/thumbor/7CwCuGAKxTrQ4wPyOBpKjSsd1JI/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/04/19/743/n/41542884/5429b59c8e78fbc4_MCDTITA_FE014_H_1_.JPG'
-    )
-
     # Display the title
     with overview:
         st.title(title)
+        # Display an image
+        st.image(
+            'bank.png'
+        )
 
     # Input fields for user data
     with left:
-        job_radio = st.radio('Job', list(job_d.keys()), format_func=lambda x: job_d[x])
-        education_radio = st.radio('Education', list(education_d.keys()), format_func=lambda x: education_d[x])
-        default_radio = st.radio('Default', list(default_d.keys()), format_func=lambda x: default_d[x])
-        housing_radio = st.radio('Housing', list(housing_d.keys()), format_func=lambda x: housing_d[x])
-        loan_radio = st.radio('Loan', list(loan_d.keys()), format_func=lambda x: loan_d[x])
-        contact_radio = st.radio('Contact', list(contact_d.keys()), format_func=lambda x: contact_d[x])
-        month_radio = st.radio('Month', list(month_d.keys()), format_func=lambda x: month_d[x])
-        poutcome_radio = st.radio('Poutcome', list(poutcome_d.keys()), format_func=lambda x: poutcome_d[x])
+        job_radio = st.radio('Job',
+                             list(app_inputs['job_d'].keys()),
+                             format_func=lambda x: app_inputs['job_d'][x])
+
+        education_radio = st.radio('Education',
+                                   list(app_inputs['education_d'].keys()),
+                                   format_func=lambda x: app_inputs['education_d'][x])
+
+        default_radio = st.radio('Default',
+                                 list(app_inputs['default_d'].keys()),
+                                 format_func=lambda x: app_inputs['default_d'][x])
+
+        housing_radio = st.radio('Housing',
+                                 list(app_inputs['housing_d'].keys()),
+                                 format_func=lambda x: app_inputs['housing_d'][x])
+
+        loan_radio = st.radio('Loan',
+                              list(app_inputs['loan_d'].keys()),
+                              format_func=lambda x: app_inputs['loan_d'][x])
+
+        contact_radio = st.radio('Contact',
+                                 list(app_inputs['contact_d'].keys()),
+                                 format_func=lambda x: app_inputs['contact_d'][x])
+
+        month_radio = st.radio('Month',
+                               list(app_inputs['month_d'].keys()),
+                               format_func=lambda x: app_inputs['month_d'][x])
+
+        poutcome_radio = st.radio('Poutcome',
+                                  list(app_inputs['poutcome_d'].keys()),
+                                  format_func=lambda x: app_inputs['poutcome_d'][x])
 
     with right:
         age_slider = st.slider('Age', value=50, min_value=18, max_value=95)
@@ -139,6 +165,9 @@ def main():
     ]]
 
     # Making prediction
+    # Load the model
+    file_name = 'ml_models/banking_model.h5'
+    model = pickle.load(open(file_name, 'rb'))
     accept_offer = model.predict(data)
     s_confidence = model.predict_proba(data)
 
