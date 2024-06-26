1384d2acebf5e9f21fd0648de111a8da884161a0
Marcin <s22678@pjwstk.edu.pl>
2024-06-26 21:11:53 +0200

    Add comment

--- run/run.ps1
+++ run/run.ps1
@@ -1,2 +1,3 @@
+## Powinno utworzyć folder data data i zapisywac tam pliki
 docker run -dp 0.0.0.0:8501:8501 -v $PWD/data:/app/app/data s22678suml/projekt:1.1-slim
 Start-Process "http://localhost:8501"
